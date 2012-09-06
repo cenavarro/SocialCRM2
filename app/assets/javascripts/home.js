05a2d98d95ffb2ad5a01d9929c53d5ad5aa5d93c
cenavarro <cnavarro@pernix-solutions.com>
2012-09-06 16:13:31 -0600

    Pruebas para Login con facebook

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,6 +2,6 @@ function loginFacebook(APP_ID){
   protocol = location.protocol;
   hostname = location.host;
   redirect_url = protocol+"//"+hostname+"/validate_user/";
-  url = "https://www.facebook.com/dialog/oauth?client_id="+APP_ID+"&redirect_uri="+redirect_url+"&response_type=code&display=page";
+  url = "https://www.facebook.com/dialog/oauth?client_id="+APP_ID+"&redirect_uri="+redirect_url+"&response_type=code&display=page&scope=email,manage_pages,read_insights,ads_management";
   window.location = encodeURI(url);
 }
