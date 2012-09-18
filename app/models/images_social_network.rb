f2b1ac95d7b8f57375f839ee8afa26ac2b1a1dab
cenavarro <cnavarro@pernix-solutions.com>
2012-09-18 15:37:39 -0600

    Heroku

--- app/models/images_social_network.rb
+++ app/models/images_social_network.rb
@@ -1,4 +1,6 @@
 class ImagesSocialNetwork < ActiveRecord::Base
   has_attached_file :attachment
   validates :attachment, :attachment_presence => true
+
+  attr_accessible :title, :comment, :attachment
 end
