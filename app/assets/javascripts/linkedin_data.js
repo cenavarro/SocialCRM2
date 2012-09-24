8ca024933d8ff2d7b0cccc1e81f0ae417a09ef8d
cenavarro <cnavarro@pernix-solutions.com>
2012-09-24 17:11:49 -0600

    Se arreglo el guardar mensaje de los graficos de todas las redes

--- app/assets/javascripts/linkedin_data.js
+++ app/assets/javascripts/linkedin_data.js
@@ -1,15 +1,1 @@
-function saveCommentLinkedin(form,social_network,id_comment,locale){
-  jQuery.ajax( {
-    type: 'POST',
-    url: '/'+locale+'/linkedin_data/save_comment',
-    data: { comment: $(form).find("#comment").val(), social_network: social_network, id_comment: id_comment },
-    dataType: 'json',
-    success: function(data){
-      $(form).find('.result').html(data);
-    },
-    error: function(data){
-      $(form).find('.result').html(data);
-    }
-  });
-  return false;
-}
+
--- app/assets/javascripts/twitter_data.js
+++ app/assets/javascripts/twitter_data.js
@@ -1,15 +1,1 @@
-function saveCommentTwitter(form,social_network,id_comment,locale){
-  jQuery.ajax( {
-    type: 'POST',
-    url: '/'+locale+'/twitter_data/save_comment',
-    data: { comment: $(form).find("#comment").val(), social_network: social_network, id_comment: id_comment },
-    dataType: 'json',
-    success: function(data){
-      $(form).find('.result').html(data);
-    },
-    error: function(data){
-      $(form).find('.result').html(data);
-    }
-  });
-  return false;
-}
+
