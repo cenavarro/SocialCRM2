e523d904dfad2901f1748e4f7c53e22882b56050
cenavarro <cnavarro@pernix-solutions.com>
2012-10-02 11:11:41 -0600

    Modificacion para mostrar fecha en el mes actual

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def get_start_date
+    Date.new(Time.now.strftime('%Y').to_i,Time.now.strftime('%m').to_i,1).strftime('%d-%m-%Y')
+  end
+
+  def get_end_date
+    Date.new(Time.now.strftime('%Y').to_i,Time.now.strftime('%m').to_i,-1).strftime('%d-%m-%Y')
+  end
 end
